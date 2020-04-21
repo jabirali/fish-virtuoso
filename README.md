31dfc75c2ced2a4e9358c88ebf39d07276d88011
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-04-21 19:03:50 +0200

    Feat: Initial project import.
    I have now imported the initial version from my dotfiles (gitlab.com/jabiro),
    and added a README-file to make it clearer what this does and how to use it.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# fish-virtuoso
-Integrate the fish shell with nvim/vim/vi in a smart and portable way.
+# Fish Virtuoso
+
+This plugin tries to optimize how `fish` interacts with `nvim` and `tmux`,
+with fallback functionality for when you need to use `vim` or `vi`.
+
+## Motivation
+
+If you want the following functionality, then this plugin is for you:
+
+- Automatically select the best available `$EDITOR` when `fish` starts.
+  I realize that "best" is very subjective, but in my opinion the order
+  should be `nvr`, `nvim`, `vim`, `vi`. The options to the left are used 
+  if available, but more hosts are likely to have the ones on the right.
+- Provide the same editing commands in the shell as in `vim` command mode.
+  In other words, the commands `edit`, `split`, `vsplit`, and `tabedit`
+  (and their abbreviations `e`, `sp`, `vs`, `tabe`) should do the same 
+  thing as the corresponding commands in `vim`. If your `$EDITOR` is 
+  `nvr`, these actions may be performed in an existing `nvim` instance.
+- Integrate the excellent [NeoVim Remote][1] with `tmux`. The assumption 
+  here is that if you use `nvr` as your `$EDITOR` within `tmux`, you only 
+  want *one* `nvim` instance per `tmux` session and window. Thus, typing
+  `split /etc/fstab` in a `tmux` window that has an `nvim` instance visible
+  will open the file `/etc/fstab` in a new `:split` in that `nvim` instance.
+- To help you start typing `e` or `edit` instead of `nvr`/`nvim`/`vim`/`vi`,
+  since the latter are inherently less portable, I have also added some
+  abbreviations that help with the transition. If anyone is offended by
+  this, please let me know, and I can add an option to disable these...
+
+[1]: https://github.com/mhinz/neovim-remote
+
+## Quickstart
+
+The easiest way to install this is using [fisher][2]:
+
+	fisher add jabirali/fish-virtuoso
+
+Restart `fish`, and everything should work by itself. 
+If it doesn't, feel free to open an issue on Github!
+
+[2]: https://github.com/jorgebucaran/fisher
---
+++ conf.d/virtuoso.fish
@@ -0,0 +1,90 @@
+# Autoselect the best $EDITOR {{{
+	if type -q nvr
+		if [ -n "$TMUX" ]
+			set -x EDITOR nvr
+		else
+			set -x EDITOR nvim
+		end
+	else if type -q nvim
+		set -x EDITOR nvim
+	else if type -q vim
+		set -x EDITOR vim
+	else
+		set -x EDITOR vi
+	end
+# }}}
+
+# Simplify the use of $EDITOR {{{
+	if [ "$EDITOR" = "nvr" ]
+		# When the $EDITOR is set to `nvr`, we can define different functions 
+		# for accessing a file via `:edit`, `:split`, `:vsplit`, and `:tabedit`.
+		function edit -w nvr
+			nvr $argv 2> /dev/null
+		end
+		
+		function split -w nvr
+			nvr -cc split $argv 2> /dev/null
+		end
+		
+		function vsplit -w nvr
+			nvr -cc split $argv 2> /dev/null
+		end
+		
+		function tabedit -w nvr
+			nvr -cc split $argv 2> /dev/null
+		end
+	else
+		# When the $EDITOR is not `nvr`, the best we can do is to is to open the
+		# specified files in a new instance as separate splits, vsplits, or tabs.
+		function edit -w $EDITOR
+			$EDITOR $argv
+		end
+		
+		function split -w $EDITOR
+			$EDITOR -o $argv
+		end
+		
+		function vsplit -w $EDITOR
+			$EDITOR -O $argv
+		end
+		
+		function tabedit -w $EDITOR
+			$EDITOR -p $argv
+		end
+	end
+	
+	# Replicate the Vim short-hand commands.
+	abbr -ga e  edit
+	abbr -ga sp split
+	abbr -ga vs vsplit
+	abbr -ga tabe tabedit
+	
+	# Force the use of $EDITOR.
+	abbr -ga vi   edit
+	abbr -ga vim  edit
+	abbr -ga nvim edit
+	abbr -ga nvr  edit
+# }}}
+
+# Integrate Tmux and Neovim {{{
+	# This is done by syncing the Neovim instance to the Tmux window, so
+	# running `nvr` reuses a currently visible Neovim instance. Placing
+	# this in the prompt keeps it up-to-date after moving panes around.
+	if [ -n "$TMUX" ]
+		# Ensure that the cache directory exists.
+		mkdir -p ~/.cache/nvim
+		
+		# Make a backup of the existing prompt.
+		functions -c fish_prompt _nvr_old_prompt
+		
+		# Update the prompt to check `tmux` window. This is done to ensure that 
+		# the window information remains up to date even if the pane is relocated.
+		function fish_prompt
+			# Sync the `nvim` instance to the `tmux` session and window.
+			set -gx NVIM_LISTEN_ADDRESS (tmux display -p '#{HOME}/.cache/nvim/nvr#{session_id}#{window_id}')
+			
+			# Restore the old prompt.
+			_nvr_old_prompt
+		end
+	end
+# }}}
